0335e90b3cfff3a71867db59e3d170449e32f6e9
Anurag4734 <anuragkarki4734@gmail.com>
2022-08-09 21:02:14 +0545

    Initial commit

---
+++ index.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Hello World </title>
+    </head>
+    <body>
+        <?php
+        $name="Anurag";
+        echo "Hello World . I am " .$name;
+        ?>
+    </body>
+</html>
+
+    
